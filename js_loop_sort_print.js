fae8912da87ecc51ab31312bc9658fb46ce80cff
Reshad94 <32731514+Reshad94@users.noreply.github.com>
2018-08-01 14:40:34 +0400

    Create js_loop_sort_print.js

---
+++ js_loop_sort_print.js
@@ -0,0 +1,19 @@
+function array_sort_desc(name,amount) {
+  var tempVar, tempVar1
+for (var i = 0; i < amount.length - 1; i++)
+{
+       for(var j = 0; j < amount.length - i - 1; j++)
+       {
+                if(parseFloat(amount[j]) < parseFloat(amount[j + 1]))
+                {
+                            tempVar = amount[j];
+                            amount[j]= amount[j + 1];
+                            amount[j + 1] = tempVar;
+//
+                            tempVar1 = name[j];
+                            name[j]= name[j + 1];
+                            name[j + 1] = tempVar1;
+                }
+       }
+}
+}
