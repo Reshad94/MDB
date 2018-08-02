0a07faa43e587843cd2ca2001b1200ed0130e24d
Reshad94 <32731514+Reshad94@users.noreply.github.com>
2018-08-02 20:38:21 +0400

    Update js_loop_sort_print.js

--- js_loop_sort_print.js
+++ js_loop_sort_print.js
@@ -39,3 +39,19 @@ if(mm<10) {
 today = dd + '.' + mm + '.' + yyyy;
   return today;
 }
+//----------------------------------------------------
+function date_dif(date1, date2)
+{
+   //Get 1 day in milliseconds
+  var one_day=1000*60*60*24;
+
+  // Convert both dates to milliseconds
+  var date1_ms = date1.getTime();
+  var date2_ms = date2.getTime();
+
+  // Calculate the difference in milliseconds
+  var difference_ms = date2_ms - date1_ms;
+    
+  // Convert back to days and return
+  return Math.round(difference_ms/one_day); 
+}
