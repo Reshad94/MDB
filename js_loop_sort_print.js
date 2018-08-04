309414bfb53ee6cd4bc27bc5483f473e49c23142
Reshad94 <32731514+Reshad94@users.noreply.github.com>
2018-08-04 18:30:56 +0400

    Update js_loop_sort_print.js

--- js_loop_sort_print.js
+++ js_loop_sort_print.js
@@ -56,9 +56,12 @@ function date_dif(date1, date2)
   return Math.round(difference_ms/one_day); 
 }
 //---------------------------------------
-function sum_sync(arr1, arr2, arr3) {
+function sum_sync(arr1, arr2, arr3, arr4) {
 for (var e = 0; e < arr1.length; e++) 
 {
-  arr3[arr1[e] - 1] += parseFloat(arr2[e]);
+  if (arr1[e] > 34)
+  {
+  arr4[arr2[e] - 1] += parseFloat(arr3[e]);
+  }
 }
 }
