d65a72f5936f2d1fa8420bc3143afc265d28a6f6
Reshad94 <32731514+Reshad94@users.noreply.github.com>
2018-08-05 21:51:07 +0400

    Update f_c_l.js

--- f_c_l.js
+++ f_c_l.js
@@ -9,7 +9,9 @@ var f125 = 12.5/36000, f13 = 13/36000, new_datedifs = parseInt(0);
 var ids = [], dates = [], cids = [], credits = [], ccredits = [], c_ccredits = [], cdates = [], datedifs = [], anpmts = [], amountdifs = [];
 array_inisial_zero(c_ccredits, 34);
 
+   var iii = 0;
 for (var e = 0; e < ent.length; e++) 
+{
   if (
     (
       moment(date_t(ent[e].field("Date"))).toDate() <= moment(begin_now).toDate()
@@ -23,7 +25,7 @@ for (var e = 0; e < ent.length; e++)
      )
     //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
   {
-{
+
 ids.push(parseInt(ent[e].field("Id")));
 dates.push(date_t(ent[e].field("Date")));
 cids.push(parseInt(ent[e].field("c_id")));
@@ -34,9 +36,9 @@ amountdifs.push(0.00);
 datedifs.push(0);
 if (ids[e] < 34)
 {
-if (moment(dates[e]).toDate() > moment(begin_now).toDate())
+if (moment(dates[iii]).toDate() > moment(begin_now).toDate())
 {
-cdates.push(dates[e]);
+cdates.push(dates[iii]);
 }
 else
 {
@@ -45,16 +47,19 @@ cdates.push(date_now());
 }
 else
 {
-cdates.push(dates[e]);
+cdates.push(dates[iii]);
 }
-datedifs[e] = date_dif(cdates[e], enddates);
+datedifs[iii] = date_dif(cdates[iii], enddates);
+   iii += 1;
 }
     //@@@@@@@@@@@@@@@@@@@@@@@@@@
   }
 
 sum_sync(ids, cids, credits, c_ccredits);
 
+   var jjj = 0;
 for (var e = 0; e < ent.length; e++) 
+{
     if (
     (
       moment(date_t(ent[e].field("Date"))).toDate() <= moment(begin_now).toDate()
@@ -68,26 +73,27 @@ for (var e = 0; e < ent.length; e++)
      )
     //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
   {
-{
+
 if (ent[e].field("Id") <= 34)
 {
-ccredits[e] = parseFloat(ent[e].field("Credit")) - parseFloat(c_ccredits[parseInt(ent[e].field("c_id")) - 1]);
+ccredits[jjj] = parseFloat(ent[e].field("Credit")) - parseFloat(c_ccredits[parseInt(ent[e].field("c_id")) - 1]);
 }
 else
 {
-ccredits[e] = ent[e].field("Credit");
+ccredits[jjj] = ent[e].field("Credit");
 }
 
-if (moment(dates[e]).toDate() > moment(x).toDate())
+if (moment(dates[jjj]).toDate() > moment(x).toDate())
 {
-anpmts[e] = Math.pow(1 + f13, datedifs[e]) * ccredits[e];
+anpmts[jjj] = Math.pow(1 + f13, datedifs[jjj]) * ccredits[jjj];
 }
 else
 {
-new_datedifs = date_dif(cdates[e], y);
-anpmts[e] = (Math.pow(1 + f125, new_datedifs + 1) + Math.pow(1 + f13, datedifs[e] - new_datedifs - 1) ) * ccredits[e];
+new_datedifs = date_dif(cdates[jjj], y);
+anpmts[jjj] = (Math.pow(1 + f125, new_datedifs + 1) + Math.pow(1 + f13, datedifs[jjj] - new_datedifs - 1) ) * ccredits[jjj];
 }
-amountdifs[e] = anpmts[e] - ccredits[e];
+amountdifs[jjj] = anpmts[jjj] - ccredits[jjj];
+   jjj += 1;
 //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 }
 }
