582f80bacbaf3627d7ffe5811fd70e174bc694db
Reshad94 <32731514+Reshad94@users.noreply.github.com>
2018-08-08 17:37:14 +0400

    Update f_c_l.js

--- f_c_l.js
+++ f_c_l.js
@@ -103,10 +103,10 @@ amountdifs[jjj] = anpmts[jjj] - ccredits[jjj];
 }
 
    //message (moment(begin_now).format("DD-MM-YYYY"));
-   f_print_each(ids, dates, credits, cdates, ccredits, cids, datedifs, anpmts, amountdifs, 
-"Id     Dates     Credit     cDate     cCredit     cid     DateDif     a(n)*PMT     AmountDif",
-                moment(begin_now).format("DD-MM-YYYY")
-               );
+//   f_print_each(ids, dates, credits, cdates, ccredits, cids, datedifs, anpmts, amountdifs, 
+//"Id     Dates     Credit     cDate     cCredit     cid     DateDif     a(n)*PMT     AmountDif",
+   //             moment(begin_now).format("DD-MM-YYYY")
+         //      );
    
    
    
