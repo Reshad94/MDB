1b4b6029632865dc7fc140ca96023945eaf3dbc8
Reshad94 <32731514+Reshad94@users.noreply.github.com>
2018-08-28 16:43:25 +0400

    Update js_loop_sort_print.js

--- js_loop_sort_print.js
+++ js_loop_sort_print.js
@@ -127,10 +127,10 @@ function sum_with_date_txt(arr_a, arr_d, datee) {
 var sum = 0.00;
 	f = file("/sdcard/memento/compare/test-" + moment(datee).format("DD-MM-YYYY") + " .txt");
 for (var i = 0; i < arr_a.length; i++) {
-	if (moment(arr_d[i]).toDate() == moment(datee).toDate())
+//	if (moment(arr_d[i]).toDate() == moment(datee).toDate())
 	    {
   sum += arr_a[i];
-		   f.writeLine("i = " + i + "   arr_a = " + arr_a[i].toFixed(2) + "   sum = " + sum.toFixed(2)); 
+		   f.writeLine("i = " + i + "   arr_a = " + arr_a[i].toFixed(2) + "   sum = " + sum.toFixed(2)+ "   arr_d = " + arr_d[i]); 
 	    }
 }
 			   f.close();
